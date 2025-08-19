9b8a783eb9e5dfcfa97b647e5fa9c6782a7b6c33
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-08-19 23:42:37 +0800

    new upd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/movies-trend"
+  base: process.env.VITE_BASE_PATH ||"/movies-trend"
 })
